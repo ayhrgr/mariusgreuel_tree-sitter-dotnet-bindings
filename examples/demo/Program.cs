e9207a9fbfc2ee30d23fd6f13351974212252392
ayhrgr <ayhrgr@gmail.com>
2025-06-02 17:27:51 +0900

    Add additional examples for parsing C# classes and methods

--- examples/demo/Program.cs
+++ examples/demo/Program.cs
@@ -17,3 +17,27 @@
         Console.WriteLine($"Found function: {capture.Node.Text}");
     }
 }
+
+{
+    using var language = new Language("tree-sitter-c-sharp", "tree_sitter_c_sharp");
+    using var parser = new Parser(language);
+    using var tree = parser.Parse("class Hello { static void Main() { System.Console.WriteLine(\"Hello, C#\"); } }")!;
+    Console.WriteLine($"Root node: {tree.RootNode}");
+    using var query = new Query(language, "(method_declaration name: (identifier) @fn)");
+    foreach (var capture in query.Execute(tree.RootNode).Captures)
+    {
+        Console.WriteLine($"Found function: {capture.Node.Text}");
+    }
+}
+
+{
+    using var language = new Language("tree-sitter-c-sharp", "tree_sitter_c_sharp");
+    using var parser = new Parser(language);
+    using var tree = parser.Parse("class Hello { static void Main() { System.Console.WriteLine(\"Hello, C#\"); } }")!;
+    Console.WriteLine($"Root node: {tree.RootNode}");
+    using var query = new Query(language, "(method_declaration name: (identifier) @name body: (block) @body)");
+    foreach (var capture in query.Execute(tree.RootNode).Captures)
+    {
+        Console.WriteLine($"Found function: {capture.Node.Text}");
+    }
+}
