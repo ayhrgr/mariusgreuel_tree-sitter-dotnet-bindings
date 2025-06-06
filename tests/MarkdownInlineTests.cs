45806226b45fdcf42c110a6a909155cfd8ab6368
ayhrgr <ayhrgr@gmail.com>
2025-06-06 17:54:53 +0900

    Add MarkdownInlineTests class with initial test setup and cleanup methods

---
+++ tests/MarkdownInlineTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeSitter.Tests;
+
+[TestClass]
+public class MarkdownInlineTests
+{
+    readonly Language _markdown = new("tree-sitter-markdown-inline", "tree_sitter_markdown_inline");
+    Parser _parser = null!;
+    Tree? _tree;
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        _parser = new Parser(_markdown);
+    }
+
+    [TestCleanup]
+    public void TestCleanup()
+    {
+        _tree?.Dispose();
+        _parser.Dispose();
+        _markdown.Dispose();
+    }
+
+    private static string RemoveWhitespace(string input)
+    {
+        return input.Replace("\n", "").Replace("\r", "").Replace(" ", "").Replace("\t", "");
+    }
+
+}
